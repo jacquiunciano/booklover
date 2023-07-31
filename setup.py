40f92588e4027dc0dd4c4b1d90432614418f02b3
jacquiunciano <jacqueline.unciano@gmail.com>
2023-07-31 15:35:25 -0400

    exercise 09 for ds5100

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="M09_exercise",
+    version="0.0.1"
+    url="https://github.com/jacquiunciano/booklover",
+    author="Jacqui Unciano",
+    author_email="jdu5sq@virginia.edu",
+    description="help keep track of books; package made under DS5100 at the University of Virginia",
+    packages=find_packages()
+)
